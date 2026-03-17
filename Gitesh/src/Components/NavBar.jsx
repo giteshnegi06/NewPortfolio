09bb5a7600c33700739acd6373d71b6f78a0b8a5
Giglo01 <negigitesh@gmail.com>
2026-03-17 22:32:25 +0530

    asd

--- Gitesh/src/Components/NavBar.jsx
+++ Gitesh/src/Components/NavBar.jsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import { CgMenuLeft } from "react-icons/cg";
 
 export default function NavBar() {
 
@@ -19,7 +20,7 @@ export default function NavBar() {
                         <h1 className='text-2xl font-smithen tracking-wide'>
                             Gitesh <span className="bg-linear-to-r from-[#f68b43] via-[#bf72a2] to-[#a164d6] bg-clip-text text-transparent text-2xl font-smithen tracking-wide">Kumar</span>
                         </h1></div>
-                    <ul className='flex justify-between items-center w-108 text-sm text-[#5f5f70] font-bold '>
+                    <ul className='hidden lg:flex justify-between items-center w-108 text-sm text-[#5f5f70] font-bold '>
                         {navMenu.map((v, i) => (
                             <li key={i}><a href={v.link} className=' hover:text-white duration-300'>{v.name}</a></li>
                         ))}
@@ -27,6 +28,10 @@ export default function NavBar() {
                             Hire Me
                         </button>
                     </ul>
+
+                    <div className='lg:hidden'>
+                        <CgMenuLeft size={28} />
+                    </div>
                 </nav>
             </header>
         </>
